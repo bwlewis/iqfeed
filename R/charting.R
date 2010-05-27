52ffa93e5b6dce99a5d425c7b510d2cae56f4bcd
B. W. Lewis <bwaynelewis@gmail.com>
2010-05-26 20:10:10 -0400

    Added a simple quantmod-style charting function

---
+++ R/charting.R
@@ -0,0 +1,21 @@
+crapmod = function(x, title="", col=4, lwd=2, lty=1)
+{
+  p = as.numeric(x[,1])
+  plot.new()
+  par(mar=c(2.2,3,3,3))
+  plot(p, axes=FALSE, type='l', col="#eeeeee",ylab="Close", main=title, xlab='')
+  box()
+  axis(side=2,pretty(p))
+  axis(side=4,pretty(p))
+  at = endpoints(x,on="days") + 1 
+  at = at[-length(at)]
+  D = unique(format(index(x)[at],"%h %d"))
+  axis(side=1,at=at,labels=D)
+  abline(v=at,lwd=1,col="#aabbff")
+  abline(h=pretty(p),lwd=1,col="#aabbff")
+  for(j in D){
+    i <- which((format(index(x),"%h %d")==j))
+    y <- as.numeric(x[i,'Close'])
+    lines(i, y, col=col, lwd=lwd, lty=lty)
+  }
+}
