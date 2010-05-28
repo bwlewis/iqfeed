82c86f3a289c9d50afa9382dcea0a6e0132319db
B. W. Lewis <bwaynelewis@gmail.com>
2010-05-28 19:16:18 -0400

    Improved charting a bit for long series

--- R/charting.R
+++ R/charting.R
@@ -1,20 +1,44 @@
-crapmod = function(x, title="", col=4, lwd=2, lty=1)
+crapmod = function(x, title="", col=4, lwd=2, lty=1, xlim=NULL, ylim=NULL)
 {
   p = as.numeric(x[,1])
   plot.new()
   par(mar=c(2.2,3,3,3))
-  plot(p, axes=FALSE, type='l', col="#eeeeee",ylab="Close", main=title, xlab='')
+  plot(p, axes=FALSE, type='l', col="#eeeeee",ylab="Close", main=title, xlab='',
+       xlim=xlim, ylim=ylim)
   box()
   axis(side=2,pretty(p))
   axis(side=4,pretty(p))
   at = endpoints(x,on="days") + 1 
-  at = at[-length(at)]
+  if(at[length(at)] > length(x))
+    at = at[-length(at)]
   hat = endpoints(x,on="hours") + 1 
-  hat = hat[-length(at)]
+  if(hat[length(hat)] > length(x))
+    hat = hat[-length(at)]
+  mat = endpoints(x,on="months") + 1 
+  if(mat[length(mat)] > length(x))
+    mat = mat[-length(mat)]
+  yat = endpoints(x,on="years") + 1 
+  if(yat[length(yat)] > length(x))
+    yat = yat[-length(yat)]
+  abline(h=pretty(p),lwd=1,col="#aabbff")
+  if(length(mat)>0) {
+# Plot a whole year or more of data
+    M = format(index(x)[mat],"%h")
+    axis(side=1,at=mat,labels=FALSE)
+    abline(v=mat,lwd=1,col="#aabbff")
+    mtext(M, side=1,at=mat,line=0,cex=0.75)
+    if(length(yat)>0) {
+      Y = format(index(x)[yat],"%Y")
+      mtext(Y, side=1,at=yat,line=1,cex=1)
+    }
+    if(length(M)<4)
+      axis(side=1,at=at,labels=FALSE,col="#555555",lwd.ticks=1)
+    lines(1:length(index(x)),x,col=col,lwd=lwd,lty=lty)
+    return(invisible())
+  }
   D = unique(format(index(x)[at],"%h %d"))
   axis(side=1,at=at,labels=D)
   abline(v=at,lwd=1,col="#aabbff")
-  abline(h=pretty(p),lwd=1,col="#aabbff")
   if(length(D)<2) {
     H = unique(format(index(x)[hat],"%H:%M"))
     mtext(H, side=1,at=hat,line=0,cex=0.75)
@@ -25,5 +49,6 @@ crapmod = function(x, title="", col=4, lwd=2, lty=1)
     y <- as.numeric(x[i])
     lines(i, y, col=col, lwd=lwd, lty=lty)
   }
-  axis(side=1,at=hat,labels=FALSE,col="#555555",lwd.ticks=1)
+  if(length(D)<7)
+    axis(side=1,at=hat,labels=FALSE,col="#555555",lwd.ticks=1)
 }
